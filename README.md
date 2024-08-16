7fe6d3fa2dab98c52cac08141ebc5bfb5db01977
Gerard <air.angles@gmail.com>
2024-08-16 12:07:42 +0200

    add descriptions in fun_dual, description in readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # SDP_bounds_for_quantum_codes
+
+Denote by (($n$,$K$,$\delta$)) a qubit quantum code with distance \delta, which encodes a K-dimensional Hilbert space to a n qubit Hilbert space 
+  
+This library provides a solution with a positive objective function of the SDP in Eq.(148) for (($7$,$1$,$4$)) and of the SDP in Eq.(150) for (($8$,$9$,$3$)) and (($10$,$5$,$4$)).
+
+The solutions in pickle format: inf\_cert\_7, inf\_cert\_8 and inf\_cert\_10. 
+
+
+The programs certificate\_7.ipynb, certificate\_8.ipynb and certificate\_10.ipynb read the pickle files and check the objective function and the violation constraints for each code respectivly.
+
+This proof the non-existence of qubit quantum codes (($7$,$1$,$4$)), (($8$,$9$,$3$)) and (($10$,$5$,$4$)).
+
+We additionally provide lovasz_number.ipynb which checks that the quantum lovasz number written as in Eq.(157), disproof the existence of the qubit quantum code (($4$,$1$,$3$)).
+  
--- fun_dual.py
+++ fun_dual.py
@@ -2,29 +2,37 @@ import mpmath as mp
 from itertools import product
 
 # binomials and coefficients:
-
 def comb(n, k):
+    """
+    returns the binomial coefficients
+    """
 
     value = mp.binomial(n, k)
     return int(value)
 def multinomial(n,i,j,k,l):
-
+    """
+    returns the multinomial coefficients defined in Eq.(77)
+    """
     value=mp.factorial(n)/(mp.factorial(i)*mp.factorial(j)*mp.factorial(k)*mp.factorial(l)*mp.factorial(n-i-j-k-l))
 
     return int(value)
 def gamma(i,j,t,p,n):
+    """
+    returns the gamma coefficients defined in Eq.(76)
+    """
+
     return 3**(i+j-t)*2**(t-p)*multinomial(n,p,t-p,i-t,j-t)
 def beta(i,j,k,m,t):
+    """
+    returns the beta coefficients defined in Eq.(83)
+    """
 
-	b=0
+    return sum([comb(u,t)*comb(m-2*k,u-k)*comb(m-k-u,i-u)*comb(m-k-u,j-u)*(-1)**(t-u) for u in range(m+1)])
 
-	for u in range(m+1):
-
-		b+= comb(u,t)*comb(m-2*k,u-k)*comb(m-k-u,i-u)*comb(m-k-u,j-u)*(-1)**(t-u)
-
-	return b
 def alpha(i,j,t,p,a,k,n):
-
+    """
+    returns the alpha coefficients defined in Eq.(83)
+    """
     sum=0
 
     for g in range(p+1):
@@ -32,8 +40,14 @@ def alpha(i,j,t,p,a,k,n):
 
     return beta(i-a,j-a,k-a,n-a,t-a)*3**((1/2)*(i+j)-t)*sum
 
-# mappings of i,j,t,p to 1D array and a,k to 1D array
+# mappings of (i,j,t,p) and (a,k) to 1D array.
+
 def f4_fun(n):
+    """
+    dictionary that maps a 4-array (i,j,t,p) to a 1-D array.
+    It defines the set I(q,n) in Eq.(75)
+    """
+
     funct={}
     size = 0
     for i in range(n + 1):
@@ -44,6 +58,12 @@ def f4_fun(n):
                     size += 1
     return funct, size
 def f2M_fun(n):
+    """
+    dictionary that maps a 2-array (a,k) to a 1-D array.
+    Numerates the matrices from the block-diagonalization
+    of the Terwilliger algebra [see Eq.(84)]
+    """
+
     fun={}
     count = 0
     for a in range(n+1):
@@ -54,6 +74,10 @@ def f2M_fun(n):
 
 ### dual yijtp ###
 def yijtp_fun(Y,n):
+
+    """
+    returns the dual variable yijtp defined after Eq.(164) or Eq.(174)
+    """
 
     f2M,_=f2M_fun(n)
 
@@ -70,70 +94,24 @@ def yijtp_fun(Y,n):
                 yijtp.append(y)
     return yijtp
 
-# weight
 def wt(P,n):
+
+    """
+    returns the weight of a pauli string P, e.g. 'XIZI', of size n.
+    """
     return n-list(P).count('I')
 
-def K(m,k,n,ga,de):
+def K(j,i,n):
+    """
+    returns the quarternary Krawtchouk Polynomial as defined in Eq.(16) .
+    """
     sum=0
     for alp in range(n+1):
-        sum += (-1)**alp*comb(n-k,m-alp)*comb(k,alp)*ga**((n-k)-(m-alp))*de**(m-alp)
+        sum += (-1)**alp*comb(n-i,j-alp)*comb(i,alp)*3**(j-alp)
     return sum
 
 def D_fun(C,n,d):
-    return [2 ** (-n) * sum(K(j, i, n, 1, 3) * C[j] for j in range(d)) for i in range(n + 1)]
-
-# import pickle
-#
-# n,d=7,4
-# decimals=9
-#
-# with open("inf_cert", 'rb') as inf_cert:
-#     Y=pickle.load(inf_cert)
-#
-# y=yijtp_fun(Y,n)
-#
-# f4,_=f4_fun(n)
-#
-# ### obj value ###
-#
-# c = 2*y[f4[(n, 0, 0, 0)]]/gamma(n, 0, 0, 0, n) + y[f4[n, n, n, n]]/gamma(n, n, n, n, n)
-#
-# obj_val = -y[f4[0, 0, 0, 0]]/gamma(0, 0, 0, 0, n) - (2**n-1)*c
-#
-# print("obj_val=%s" % obj_val)
-#
-# #### Lovasz constraints ####
-#
-#
-# const = True
-#
-# for i, j, t, p in f4.keys():
-#     if not ((t - p) % 2) == 1 and (i + j - t - p) >= d and not j == 0 and not i==0:
-#         zero = np.round(y[f4[i, j, t, p]], decimals)
-#         if zero != 0:
-#             const = False
-#             print((i, j, t, p),zero)
-#
-# print("Lovasz zeros = %s" % const)
-#
-# const = True
-#
-# for i in range(d, n + 1):
-#     zero = 2*y[f4[i, 0, 0, 0]] / gamma(i, 0, 0, 0, n) + y[f4[i, i, i, i]] / gamma(i, i, i, i, n) - c
-#     zero = np.round(zero, decimals)
-#     if zero != 0:
-#         const = False
-#         print(zero)
-#
-# print("Constraint zeros = %s" % const)
-#
-# ### minimal eigenvalue ###
-#
-# ref = np.infty
-# for i in range(len(Y)):
-#     min_eig = min(np.linalg.eigvals(Y[i].astype('float')))
-#     if ref >= min_eig:
-#         ref = min_eig
-#
-# print("Min eigenvalue = %s" % ref)
+    """
+    returns the Di function as defined after Eq.(174)) .
+    """
+    return [2 ** (-n) * sum(K(j, i, n) * C[j] for j in range(d)) for i in range(n + 1)]
