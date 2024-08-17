c1b4c7ffb92d8af4a822c70e746fc34a84e2ed84
Gerard <air.angles@gmail.com>
2024-08-17 10:01:27 +0200

    add sdp dual

--- fun_dual.py
+++ fun_dual.py
@@ -1,5 +1,8 @@
 import mpmath as mp
 from itertools import product
+import picos as pic
+from itertools import product, permutations
+import numpy as np
 
 # binomials and coefficients:
 def comb(n, k):
@@ -101,7 +104,7 @@ def wt(P,n):
     """
     return n-list(P).count('I')
 
-def K(j,i,n):
+def K_fun(j,i,n):
     """
     returns the quarternary Krawtchouk Polynomial as defined in Eq.(16) .
     """
@@ -115,3 +118,55 @@ def D_fun(C,n,d):
     returns the Di function as defined in Eq.(150)) .
     """
     return [2 ** (-n) * sum(K(j, i, n) * C[j] for j in range(d)) for i in range(n + 1)]
+
+def SDPdual(n, K, d, upperbound=None, solver='cvxopt', verbosity=0, sol_primal=True, sol_dual=True, sdptol=1e-09):
+
+    f4, size = f4_fun(n)
+    f2M, _ = f2M_fun(n)
+
+    sdp = pic.Problem(verbosity=verbosity)
+    sdp.options["*_tol"]=sdptol
+
+    Y = [pic.SymmetricVariable("Y%s%s" % (a, k), int(n + a - 2 * k + 1)) for a in range(n + 1) for k in
+         range(a, int((n + a) / 2) + 1)]
+
+    C = pic.RealVariable("C", n+1)
+    Q = pic.RealVariable("P", n + 1)
+    D = [pic.sum([2**(-n)*K_fun(j,i,n)*C[j] for j in range(d)]) for i in range(n+1)]
+
+    y = yijtp_fun(Y, n)
+
+    sdp.add_list_of_constraints(Y[i] >> 0 for i in range(len(Y)))
+
+    obj_fun = (K*D[0]-C[0])+(2**n/K-1)*Q[0] - y[f4[0, 0, 0, 0]]
+
+    sdp.set_objective('max', obj_fun)
+
+    if upperbound != None:
+        sdp.add_constraint(obj_fun <= upperbound)
+
+    #### Constraints ####
+
+    sdp.add_list_of_constraints(
+        -2*y[f4[i, 0, 0, 0]]-y[f4[i, i, i, i]] + K*D[i]-C[i]+(2**n/K-2)*Q[i]-Q[0]==0
+        for i in range(1,d))
+
+    sdp.add_list_of_constraints(
+        -2*y[f4[i, 0, 0, 0]]-y[f4[i, i, i, i]] + (K*D[i]+(2**n/K-2)*Q[i]-Q[0])==0
+        for i in range(d,n+1))
+
+    sdp.add_list_of_constraints(
+        pic.sum([y[f4[a, b, c, q]] + Q[a+b-c-q] for a, b, c, q in f4.keys()
+                 if t - p == c - q and (i, j, i + j - t - p) in list(permutations((a, b, a + b - c - q)))])==0
+    for i, j, t, p in f4.keys() if (t - p) % 2 == 0 and not j == 0 and not i==j==t==p)
+
+    sol = sdp.solve(solver=solver, primals=sol_primal, duals=sol_dual)
+
+    Y = [np.array(Y[i].value) if np.array(Y[i].value).size != 1 else np.array([[Y[i].value]]) for i in
+                range(len(Y))]
+
+    C, Q = C.value, Q.value
+    obj=sdp.objective.value
+    sol= sol.problemStatus
+
+    return sol, Y, C, Q, f4, obj
